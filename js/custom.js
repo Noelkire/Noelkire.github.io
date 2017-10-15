fb41b2cf00db4bac229e9b2e4ef1c4ae2e378838
Noelkire <erikgleon@gmail.com>
2017-10-14 22:58:09 -0700

    ronel
    robel

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,6 @@ $(document).ready(function() {
 				sectionsColor: ['#e5e5e5', '#222222', '#e5e5e5', '#222222', '#e5e5e5'],
 				anchors: ['firstPage', '2ndPage', '3rdPage', '4thPage', '5thPage'],
 				menu: '#menu',
-				scrollingSpeed: 500
+				scrollingSpeed: 1000,
 			});
 		});
