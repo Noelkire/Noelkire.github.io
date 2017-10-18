e229e2345a35e0ea15e53f8479c36b66d72cd50b
Noelkire <erikgleon@gmail.com>
2017-10-18 02:48:09 -0700

    fix
    f

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+			"use strict";
 			$('#fullpage').fullpage({
 				sectionsColor: ['#e5e5e5', '#222222', '#e5e5e5', '#222222', '#e5e5e5'],
 				anchors: ['firstPage', '2ndPage', '3rdPage', '4thPage', '5thPage'],
