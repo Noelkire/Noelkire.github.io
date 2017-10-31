8cf68841c0b2334c6c7dd0c4d7d75fa0e6b74633
Noelkire <erikgleon@gmail.com>
2017-10-31 02:30:31 -0700

    we
    ds

--- js/custom.js
+++ js/custom.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 			$('#fullpage').fullpage({
 				sectionsColor: ['#e5e5e5', '#222222', '#e5e5e5', '#222222', '#e5e5e5'],
 				anchors: ['firstPage', '2ndPage', '3rdPage', '4thPage', '5thPage'],
+				lockAnchors: true,
 				menu: '#menu',
 				scrollingSpeed: 1000,
 				responsiveWidth: 900,
