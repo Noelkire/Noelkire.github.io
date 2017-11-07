39f3e91cf68fb4701affd9229b9c42d2c4f16a61
Noelkire <erikgleon@gmail.com>
2017-11-07 10:02:16 -0800

    d
    d

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
 			"use strict";
 			$('#fullpage').fullpage({
-				sectionsColor: ['#f7f7f7', '#222222', '#f7f7f7', '#222222', '#f7f7f7'],
-				anchors: ['firstPage', '2ndPage', '3rdPage', '4thPage', '5thPage'],
+				sectionsColor: ['#f7f7f7', '#000000', '#f7f7f7', '#000000', '#f7f7f7','#000000'],
+				anchors: ['firstPage', '2ndPage', '3rdPage', '4thPage', '5thPage', '6thPage'],
 				lockAnchors: true,
 				menu: '#menu',
 				scrollingSpeed: 1000,
